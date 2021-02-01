dabaaa58be79925e6d11776cb4aaf4cc8d146143
Dmitry Marakov <dmarakov@devexperts.com>
2021-02-01 22:17:51 +0300

    Day2Task1 Done

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Сколько этажей?");
+        int num = in.nextInt();
+        if (num>=1 && num <=4){
+            System.out.println("Малоэтажный дом");
+        } else if(num>=5 && num <=8){
+            System.out.println("Среднеэтажный дом");
+        } else if (num>=9){
+            System.out.println("Многоэтажный дом");
+        } else if (num<=0){
+            System.out.println("Ошибка воода");
+        }
     }
 }
