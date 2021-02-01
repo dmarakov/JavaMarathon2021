180b338c38324244bb2a8d6ee64c316a4a0fea3b
Dmitry Marakov <dmarakov@devexperts.com>
2021-02-01 20:06:37 +0300

    All tasks

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -4,7 +4,7 @@ public class Task1 {
     public static void main(String[] args) {
         int i = 0;
         while(i<10){
-            System.out.println("JAVA ");
+            System.out.print("JAVA ");
             i++;
         }
 
