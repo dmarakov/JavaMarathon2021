869195a8c6b2bdc19cf9d474f6f7d01b4ebe9e65
Dmitry Marakov <dmarakov@devexperts.com>
2021-02-09 20:51:53 +0300

    Day4Task2

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -8,7 +8,9 @@ public class Task2 {
         Random rand = new Random();
         int array[] = new int[100];
         int max = array[0];
+        int zero = 0;
         int minValue = 10000;
+        int zeroSum = 0;
 
         for (int x : array) {
             array[x] = rand.nextInt(10000);
@@ -17,12 +19,17 @@ public class Task2 {
                 max = array[x];
             if (array[x] < minValue)
                 minValue = array[x];
+            if (array[x] % 10 == 0){
+                zero = zero+1;
+                zeroSum = zeroSum + array[x];
+            }
 
 
-            System.out.println(array[x]);
+
         }
-        System.out.println("Max= " + max);
-        System.out.println("Min= " + minValue);
-
+        System.out.println("Наибольший элемент массива: " + max);
+        System.out.println("Наименьший элемент массива: " + minValue);
+        System.out.println("Количество элементов массива, оканчивающихся на 0: "+zero);
+        System.out.println("Сумма элементов массива, оканчивающихся на 0: "+zeroSum);
     }
 }
