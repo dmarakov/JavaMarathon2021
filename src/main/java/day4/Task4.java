c4191e649c8f96daa1d0bca6e7dc2edf4251cb95
dmarakov <silencesewer@gmail.com>
2021-02-12 00:29:14 +0300

    Day4Task4

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -5,12 +5,27 @@ import java.util.Random;
 public class Task4 {
     public static void main(String[] args) {
         Random rand = new Random();
-        int [] sum = new int[3];
+        int sum = 0;
+        int maxRang = 0;
         int maxSum = 0;
-        int [] array = new int[100];
+        int [] array = new int[10];
         for (int i = 0; i< array.length;i++){
-            array[i] = rand.nextInt(100);
-        }
+            array[i] = rand.nextInt(1000);
+            sum = sum + array[i];
+            System.out.print(array[i]+" ");
+            if(sum>maxSum){
+                maxSum = sum;
+                maxRang = i-2;}
+            if(i%3 == 0 && i>0){
+                System.out.println("Сумма "+sum);
+                sum = 0;
 
+
+            }
+            }
+        System.out.println();
+        System.out.println("Сумма " +maxSum);
+        System.out.println("Индекс "+maxRang);
+        }
     }
-}
+
