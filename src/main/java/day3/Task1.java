e0d4a1c7012a556ae653b0f342c2fd270b207718
dmarakov <silencesewer@gmail.com>
2021-02-25 11:09:44 +0300

    Add while to Day3Task1

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -4,25 +4,30 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String city = scanner.nextLine();
-        switch (city){
-            case "Москва", "Владивосток", "Ростов" :
-                System.out.println("Россия");
-                break;
-            case "Рим", "Милан", "Турин" :
-                System.out.println("Италия");
-                break;
-            case "Ливерпуль", "Манчестер", "Лондон" :
-                System.out.println("Англия");
-                break;
-            case "Берлин", "Мюнхен", "Кёльн" :
-                System.out.println("Германия");
-                break;
-            default:
-                System.out.println("Неизвестная страна");
-        }
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String city = scanner.nextLine();
+            switch (city) {
+                case "Москва", "Владивосток", "Ростов":
+                    System.out.println("Россия");
+                    continue;
+                case "Рим", "Милан", "Турин":
+                    System.out.println("Италия");
+                    continue;
+                case "Ливерпуль", "Манчестер", "Лондон":
+                    System.out.println("Англия");
+                    continue;
+                case "Берлин", "Мюнхен", "Кёльн":
+                    System.out.println("Германия");
+                    continue;
+                case "Stop":
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+                    continue;
 
+            }break;
+        }
 
 
     }
