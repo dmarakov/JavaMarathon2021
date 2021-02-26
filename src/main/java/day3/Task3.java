3b4b71ab427a77139678f11adc04186a73fc1b37
dmarakov <silencesewer@gmail.com>
2021-02-26 22:51:30 +0300

    Add while to Day4AllTasks

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        int i = 0;
         Scanner firstNumber = new Scanner(System.in);
         Scanner secondNumber = new Scanner(System.in);
-int i = 0;
-        while(i<5) {
+        while (i < 5) {
             i++;
             double a = firstNumber.nextDouble();
             double b = firstNumber.nextDouble();
-            if ( b == 0) {
+            if (b == 0) {
                 System.out.println("Деление на 0");
                 continue;
             }
--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -8,24 +8,29 @@ public class Task4 {
         int sum = 0;
         int maxRang = 0;
         int maxSum = 0;
-        int [] array = new int[10];
-        for (int i = 0; i< array.length;i++){
-            array[i] = rand.nextInt(1000);
-            sum = sum + array[i];
-            System.out.print(array[i]+" ");
-            if(sum>maxSum){
-                maxSum = sum;
-                maxRang = i-2;}
-            if(i%3 == 0 && i>0){
-                System.out.println("Сумма "+sum);
+        int[] array = new int[100];
+
+        for (int i = 0; i < array.length; i++)
+            array[i] = rand.nextInt(10000);
+
+
+            for (int i = 0; i < array.length-2; i++) {
                 sum = 0;
-
-
+                for (int j = i; j < i + 3; j++) {
+                    sum = sum + array[j];
+                }
+                if (sum > maxSum) {
+                    maxSum = sum;
+                    maxRang = i;
+                }
             }
-            }
-        System.out.println();
-        System.out.println("Сумма " +maxSum);
+        System.out.println("Максимальная сумма " +maxSum);
         System.out.println("Индекс "+maxRang);
-        }
+
     }
+
+}
+
+
+
 
