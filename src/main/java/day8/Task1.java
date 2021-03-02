97da56d14eeea7ba2b21ac03ed1e55558e87393d
Dmitry Marakov <dmarakov@devexperts.com>
2021-03-02 19:26:10 +0300

    Day8AllTasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,25 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
 
+
+   //     StringBuilder sb = new StringBuilder();
+        String bigOne = "";
+//     for (int i = 0; i <= 20000; i++) {
+//            sb.append(i).append(" ");
+//
+//
+//       }
+
+        for (int i = 0; i <= 20000; i++) {
+            bigOne = bigOne + i + " ";
+
+        }
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+       // System.out.print(sb.toString());
+        System.out.print(bigOne);
+        System.out.println(timeConsumedMillis);
     }
 }
