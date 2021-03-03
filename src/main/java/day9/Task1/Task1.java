20609b45e8466e72bf6528239236091889363c96
Dmitry Marakov <dmarakov@devexperts.com>
2021-03-03 21:42:35 +0300

    Day8Task1

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,12 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Teacher teacher = new Teacher("Bob", "Mathematics");
+        Student student = new Student("Rob","BE11");
+        System.out.println(student.getGroupName());
+        System.out.println(teacher.getSubjectName());
+        teacher.printInfo();
+        student.printInfo();
 
     }
 }
